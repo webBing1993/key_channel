5e479e7779910fdc15cebe372545bc8136fac4f9
郑竹兵 <zhubing.zheng@fortrun.cn>
2019-05-07 14:51:45 +0800

    'element-ui-css'

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 
 import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
 import 'element-theme-chalk';
 
 Vue.use(ElementUI)
